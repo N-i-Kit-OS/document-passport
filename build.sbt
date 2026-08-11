343e115af8b0ae204eef68cc0ff6730f92d6156a
Nikitos <n.pavlove@yandex.ru>
2026-08-11 07:53:57 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "document-passport"
+version := "0.1.0"
+scalaVersion := "2.13.15"
---
+++ src/main/scala/com/documentpassport/Main.scala
@@ -0,0 +1,5 @@
+package com.documentpassport
+
+object Main extends App {
+    println("Hello, world!")
+}
